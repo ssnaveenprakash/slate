adab20fc3b4eab3fac6ecbcf628eea5a1839abb8
Varsha Basanaik <varshabasanaik@Varshas-MacBook-Pro.local>
2026-06-22 09:59:04 +0530

    Molecules added - Formfield and SearchBar

--- projects/slate/src/public-api.ts
+++ projects/slate/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/atoms/typography/typography.component';
 export * from './lib/molecules/alert/alert.component';
 export * from './lib/molecules/dropdown/dropdown.component';
+export * from './lib/molecules/form-field/form-field.component';
+export * from './lib/molecules/search-bar/search-bar.component';
